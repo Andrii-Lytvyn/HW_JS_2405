453158bbf11cab21d46f9e66925955528efdc0ea
Andrii Lytvyn <litvin.a.a@gmail.com>
2023-05-24 21:59:36 +0200

    add task

---
+++ index.js
@@ -0,0 +1,7 @@
+const obj = {
+    products: [
+        {title: "bottle of wine", price: 35, category: "wines"},
+        {title: "1984", price: 15, category: "books", author: "George Orwell"},
+        {title: "Iphone XR", price: 400, category: "phones", campera: "12MP"}
+        ]
+    };
