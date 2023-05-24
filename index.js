5d8564ac5e2fc8e9cdc87d9e115bf4944e2396a0
Andrii Lytvyn <litvin.a.a@gmail.com>
2023-05-24 22:08:54 +0200

    add solution

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const obj = {
     products: [
-        {title: "bottle of wine", price: 35, category: "wines"},
-        {title: "1984", price: 15, category: "books", author: "George Orwell"},
-        {title: "Iphone XR", price: 400, category: "phones", campera: "12MP"}
-        ]
-    };
-    const author = 
+        { title: "bottle of wine", price: 35, category: "wines" },
+        { title: "1984", price: 15, category: "books", author: "George Orwell" },
+        { title: "Iphone XR", price: 400, category: "phones", campera: "12MP" }
+    ]
+};
+const { products } = obj;
+const [, search] = products;
+const { authors } = search;
+const { author } = search;
+console.log(author);
